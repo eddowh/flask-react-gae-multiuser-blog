7558eb224dc2942ba734a046b985ff0f854a1a5b
Eddo Hintoso <eddo.hintoso@gmail.com>
2016-06-20 15:46:19 +0800

    Move blog mixins from resources to mixins module

--- application/blogs/mixins.py
+++ application/blogs/mixins.py
@@ -1,6 +1,74 @@
 # -*- coding: utf-8 -*-
 
+from collections import OrderedDict
+from datetime import datetime
+
+from flask_restful import abort
 from google.appengine.ext import ndb
+
+import urls
+from settings import TIME_FMT
+
+
+class ReactionsResourceMixin(object):
+
+    def get_reaction_context(self, reaction):
+        username = reaction.user.get().username
+        post_id = reaction.post.get().key.integer_id()
+        return OrderedDict([
+            ('user', username),
+            (
+                'user_uri',
+                urls.get_user_uri(username)
+            ),
+            (
+                'post_uri',
+                urls.get_user_blogpost_uri(username=username, post_id=post_id)
+            ),
+            ('type', reaction.type),
+            ('timestamp', datetime.strftime(reaction.timestamp, TIME_FMT)),
+        ])
+
+    def get_reactions_context(self, reactions):
+        return [
+            self.get_reaction_context(reaction)
+            for reaction in reactions
+        ]
+
+
+class PostResourceMixin(ReactionsResourceMixin):
+
+    def get_post_by_id_or_404(self, post_id):
+        key = ndb.Key('Post', int(post_id))
+        post = key.get()
+        if post is None:
+            abort(404, status=404,
+                  message="A post with that ID does not exist.")
+        else:
+            return post
+
+    def get_post_context(self, post):
+        username = post.author.get().username
+        post_id = post.key.integer_id()
+        return OrderedDict([
+            ('author', username),
+            ('id', post_id),
+            (
+                'uri',
+                urls.get_user_blogpost_uri(username=username, post_id=post_id)
+            ),
+            ('subject', post.subject),
+            ('content', post.content),
+            ('tags', sorted([pt.tag.get().name for pt in post.tags])),
+            ('reactions_count', post.reactions.count()),
+            (
+                'reactions_uri',
+                urls.get_user_blogpost_reactions_uri(username=username,
+                                                     post_id=post_id)
+            ),
+            ('created', datetime.strftime(post.created, TIME_FMT)),
+            ('last_modified', datetime.strftime(post.last_modified, TIME_FMT)),
+        ])
 
 
 class CommentReplyDeleteMixin(object):
--- application/blogs/resources.py
+++ application/blogs/resources.py
@@ -1,17 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from collections import OrderedDict
-from datetime import datetime
-
 from flask import Blueprint, request, g
-from flask_restful import Api, Resource, abort
+from flask_restful import Api, Resource
 from google.appengine.ext import ndb
 
 from auth import basic_auth
-from settings import TIME_FMT
-import urls
 
 from users.utils import get_user_by_username_or_404
+from blogs.mixins import (
+    ReactionsResourceMixin, PostResourceMixin
+)
 from blogs.models import Post, Reaction
 
 
@@ -20,67 +18,6 @@ api = Api(Blueprint('blogs', __name__), catch_all_404s=False)
 
 def post_key(name='default'):
     return ndb.Key('posts', name)
-
-
-class ReactionsResourceMixin(object):
-
-    def get_reaction_context(self, reaction):
-        username = reaction.user.get().username
-        post_id = reaction.post.get().key.integer_id()
-        return OrderedDict([
-            ('user', username),
-            (
-                'user_uri',
-                urls.get_user_uri(username)
-            ),
-            (
-                'post_uri',
-                urls.get_user_blogpost_uri(username=username, post_id=post_id)
-            ),
-            ('type', reaction.type),
-            ('timestamp', datetime.strftime(reaction.timestamp, TIME_FMT)),
-        ])
-
-    def get_reactions_context(self, reactions):
-        return [
-            self.get_reaction_context(reaction)
-            for reaction in reactions
-        ]
-
-
-class PostResourceMixin(ReactionsResourceMixin):
-
-    def get_post_by_id_or_404(self, post_id):
-        key = ndb.Key('Post', int(post_id))
-        post = key.get()
-        if post is None:
-            abort(404, status=404,
-                  message="A post with that ID does not exist.")
-        else:
-            return post
-
-    def get_post_context(self, post):
-        username = post.author.get().username
-        post_id = post.key.integer_id()
-        return OrderedDict([
-            ('author', username),
-            ('id', post_id),
-            (
-                'uri',
-                urls.get_user_blogpost_uri(username=username, post_id=post_id)
-            ),
-            ('subject', post.subject),
-            ('content', post.content),
-            ('tags', sorted([pt.tag.get().name for pt in post.tags])),
-            ('reactions_count', post.reactions.count()),
-            (
-                'reactions_uri',
-                urls.get_user_blogpost_reactions_uri(username=username,
-                                                     post_id=post_id)
-            ),
-            ('created', datetime.strftime(post.created, TIME_FMT)),
-            ('last_modified', datetime.strftime(post.last_modified, TIME_FMT)),
-        ])
 
 
 @api.resource('/posts/')
